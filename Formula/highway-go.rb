2be1879e99908928f31f7ce493700caa67280e16
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-18 12:21:06 -0700

    Brew formula update for highway-go version v0.0.2

--- Formula/highway-go.rb
+++ Formula/highway-go.rb
@@ -5,21 +5,21 @@
 class HighwayGo < Formula
   desc "The Official Sonr Highway CLI tool for building and deploying services on the Sonr network/blockchain."
   homepage "https://sonr.io"
-  version "0.0.1"
+  version "0.0.2"
   license "GPLv3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sonr-io/highway-go/releases/download/v0.0.1/highwayd-0.0.1-darwin-arm64.tar.gz"
-      sha256 "fb10cb9d46fac2404913905ab62115f8539ec84673d046fbe0e3fb5e51739ca7"
+      url "https://github.com/sonr-io/highway-go/releases/download/v0.0.2/highwayd-0.0.2-darwin-arm64.tar.gz"
+      sha256 "c6456883c139e7ed3f7909c2500074b36bea2be3917d7deac264b7f0c1c80775"
 
       def install
         bin.install "highwayd"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/sonr-io/highway-go/releases/download/v0.0.1/highwayd-0.0.1-darwin-amd64.tar.gz"
-      sha256 "4388faf5ec5532f5c68e391bfabd0d4faf0d0c7bcf8c80f65d6dcc1358b82463"
+      url "https://github.com/sonr-io/highway-go/releases/download/v0.0.2/highwayd-0.0.2-darwin-amd64.tar.gz"
+      sha256 "aeae4027e81230ee1882855834a95d8ff1d52d079edd0c6832fd9be2ed803c50"
 
       def install
         bin.install "highwayd"
@@ -29,16 +29,16 @@ class HighwayGo < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sonr-io/highway-go/releases/download/v0.0.1/highwayd-0.0.1-linux-arm64.tar.gz"
-      sha256 "ee13a0ba1ec15b14aecf6bd813a617236552d49c5b17dab40ff37e9544a2ef6c"
+      url "https://github.com/sonr-io/highway-go/releases/download/v0.0.2/highwayd-0.0.2-linux-arm64.tar.gz"
+      sha256 "17170cdf86f020a2dd10e33d5c995ca9275ef912a65483bcf352acc3b3ccf349"
 
       def install
         bin.install "highwayd"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/sonr-io/highway-go/releases/download/v0.0.1/highwayd-0.0.1-linux-amd64.tar.gz"
-      sha256 "5b0e649f460131a76b401c71999ff5527c0429d29ab26259da7cfad306170e9d"
+      url "https://github.com/sonr-io/highway-go/releases/download/v0.0.2/highwayd-0.0.2-linux-amd64.tar.gz"
+      sha256 "dc6a8ef094b3104afc9a52116f2ee8d0740add290eefe8f97001abf81b26cd6f"
 
       def install
         bin.install "highwayd"
